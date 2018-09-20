234d24e952c752162a1ff26a0471e0bdd2cd5d3b
SketchMitch <sketch0306@gmail.com>
2018-09-20 06:36:56 -0700

    Built page structure

---
+++ project_4/src/GameBoard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Row from './Row'
+
+const GameBoard = () => {
+    return (
+        <div className="GameBoard">
+            <Row/>
+            <Row/>
+            <Row/>
+            <Row/>
+        </div>
+    )
+}
+
+export default GameBoard
---
+++ project_4/src/Row.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Item from './Item'
+
+const Row = () => {
+    return (
+        <div className="Row">
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+        </div>
+    )
+}
+
+export default Row
