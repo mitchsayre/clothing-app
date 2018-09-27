9ba58c453b0a7637b13156acd4f9933c90ba3dc7
SketchMitch <sketch0306@gmail.com>
2018-09-27 07:06:49 -0700

    Added file name and file icon along with commit message

--- github-file-list/src/App.js
+++ github-file-list/src/App.js
@@ -6,10 +6,39 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <FileList />
+        <FileList files={testFiles} />
       </div>
     )
   }
 }
+
+const testFiles = [
+  {
+      id: 1,
+      name: 'src',
+      type: 'folder',
+      updated_at: "2016-07-11 21:24:00",
+      latestCommit: {
+          message: 'Initial commit'
+      }
+  },
+  {
+      id: 2,
+      name: 'tests',
+      type: 'folder',
+      updated_at: "2016-07-11 21:24:00",
+      latestCommit: {
+          message: 'Initial commit'
+      }
+  }, {
+      id: 3,
+      name: 'README',
+      type: 'file',
+      updated_at: "2016-07-18 21:24:00",
+      latestCommit: {
+          message: 'Added a readme'
+      }
+  },
+]
 
 export default App
--- github-file-list/src/FileList.js
+++ github-file-list/src/FileList.js
@@ -2,48 +2,68 @@ import React from 'react'
 import propTypes from 'prop-types'
 
 const FileList = ({ files }) => {
-    <table className="file-list">
-        <tbody>
-            {files.map(file => (
-                <tr className="flie-list-item" key={file.id}>
-                    <td className="file-name">{file.name}</td>
-                </tr>
-            ))}
-        </tbody>
-    </table>
+    return (
+        <table className="file-list">
+            <tbody>
+                {files.map(file => (
+                    <FileListItem key={file.id} file={file} />
+                ))}
+            </tbody>
+        </table>
+    )
 }
 
 FileList.propTypes = {
     files: propTypes.array
 }
 
-const testFiles = [
-    {
-        id: 1,
-        name: 'src',
-        type: 'folder',
-        updated_at: "2016-07-11 21:24:00",
-        latestCommit: {
-            message: 'Initial commit'
-        }
-    },
-    {
-        id: 2,
-        name: 'tests',
-        type: 'folder',
-        updated_at: "2016-07-11 21:24:00",
-        latestCommit: {
-            message: 'Initial commit'
-        }
-    }, {
-        id: 3,
-        name: 'README',
-        type: 'file',
-        updated_at: "2016-07-18 21:24:00",
-        latestCommit: {
-            message: 'Added a readme'
-        }
-    },
-]
+const FileListItem = ({ file }) => {
+    return (
+        <tr className="file-list-item">
+            {getFileName(file)}
+            <CommitMessage commit={file.latestCommit} />
+        </tr>
+    )
+}
+
+FileListItem.propTypes = {
+    file: propTypes.object.isRequired
+}
+
+const CommitMessage = ({ commit }) => (
+    <td className="commit-message">
+        {commit.message}    
+    </td>
+)  
+
+CommitMessage.propTypes = {
+    commit: propTypes.object.isRequired
+}
+
+function FileIcon ({ file }) {
+    let icon = 'far fa-file'
+    if(file.type === 'folder') {
+        icon = 'fa-folder'
+    }
+
+    return (
+        <td className="file-icon">
+            
+
+            <i className={`fa ${icon}`}/>
+        </td>
+    )
+}
+
+function getFileName(file) {
+    return [
+        <FileIcon file={file} key={0}/>,
+        <td className="file-name" key={1}>{file.name}</td>
+    ]
+}
+
+FileIcon.propTypes = {
+    file: propTypes.object.isRequired
+}
 
 export default FileList
