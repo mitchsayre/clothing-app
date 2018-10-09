cfdb1a3b5b898c4dd4530b247ac94b72f5e7a6c6
SketchMitch <sketch0306@gmail.com>
2018-10-09 07:05:37 -0700

    Made GameBoard a class

--- project_4/src/GameBoard.js
+++ project_4/src/GameBoard.js
@@ -1,40 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Row from './Row'
 import Item from './Item'
 
-const GameBoard = () => {
-    return (
-        <div className="GameBoard">
-            <Row>
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-            </Row>
-            <Row>
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-            </Row>
-            <Row>
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-            </Row>
-            <Row>
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-            </Row>
-        </div>
-    )
+class GameBoard extends Component {
+    render() {
+        return (
+            <div className="GameBoard">
+                <Row>
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                </Row>
+                <Row>
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                </Row>
+                <Row>
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                </Row>
+                <Row>
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                </Row>
+            </div>
+        )
+    }
 }
 
 export default GameBoard
