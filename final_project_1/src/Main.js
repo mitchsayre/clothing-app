e180b3fa7586130f01386df297feb3f589f7b67b
SketchMitch <sketch0306@gmail.com>
2018-10-23 07:08:38 -0700

    Set up the map functions of clothing dataBases for search divs

--- final_project_1/src/Main.js
+++ final_project_1/src/Main.js
@@ -4,12 +4,92 @@ import Editor from './Editor'
 import Search from './Search'
 
 class Main extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentOutfit: {
+                head: null,
+                torso: null,
+                legs: null,
+                feet: null,
+            },
+
+            clothingLibrary: {
+                outfit1: {
+                    head: null,
+                    torso: null,
+                    legs: null,
+                    feet: null,
+                },
+                outfit2: {
+                    head: null,
+                    torso: null,
+                    legs: null,
+                    feet: null,
+                },
+                outfit3: {
+                    head: null,
+                    torso: null,
+                    legs: null,
+                    feet: null,
+                },
+                outfit4: {
+                    head: null,
+                    torso: null,
+                    legs: null,
+                    feet: null,
+                },
+            },
+        }
+        this.hatsDatabase = [
+            {
+                name: 'hatName',
+                type: 'head',
+                url: 'hat.png',
+            },
+        ]
+
+        this.shirtsDatabase = [
+            {
+                name: 'shirtName',
+                type: 'torso',
+                url: 'shirt.png',
+            },
+        ]
+
+        this.pantsDatabase = [
+            {
+                name: 'PantsName',
+                type: 'legs',
+                url: 'pants.png',
+            },
+        ]
+
+        this.shoesDatabase = [
+            {
+                name: 'shoesName',
+                type: 'feet',
+                url: 'shoes.png',
+            },
+        ]
+
+    }
+
+    componentDidMount() {
+
+    }
     render() {
         return (
             <div className="Main">
                 <ControlBar />
                 <Editor />
-                <Search />
+                <Search
+                    hats={this.hatsDatabase}
+                    shirts={this.shirtsDatabase}
+                    pants={this.pantsDatabase}
+                    shoes={this.shoesDatabase}
+                />
             </div>
         )
     }
--- final_project_1/src/Search.js
+++ final_project_1/src/Search.js
@@ -1,11 +1,32 @@
 import React from 'react'
 
-const Search = () => {
+const Search = (props) => {
+    console.log(props.hats)
+    console.log(props.shirts)
+    console.log(props.pants)
+    console.log(props.shoes)
     return (
         <div className="Search">
-Search
+            <div className="hats">
+                {props.hats.map((item) => item.name)}
+            </div>
+            <div className="shirts">
+                {props.shirts.map((item) => item.name)}
+            </div>
+            <div className="pants">
+                {props.shirts.map((item) => item.name)}
+            </div>
+            <div className="shoes">
+                {props.shoes.map((item) => item.name)}
+            </div>
         </div>
     )
 }
+
+// {data.this.shirts.map((number) =>
+//     <p key={number.name}>
+//         value={number.name} </p>
+
+// )}
 
 export default Search
